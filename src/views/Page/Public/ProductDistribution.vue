00fe854c99a76f3f9a69b5cdec03970875a9797e
YQ <yq.low@wellous.com>
2023-08-30 22:28:30 +0800

    update.

--- src/views/Page/Public/ProductDistribution.vue
+++ src/views/Page/Public/ProductDistribution.vue
@@ -1,28 +1,76 @@
 <template>
-  <h1>Moon Cake Distribution System</h1>
-  <div class="flex ">
-    <div >
-      <h2>Product Input：</h2>
-      <div class="flex m-1" v-for="productIndex of Object.keys(itemQuantities)">
-        <label class="w-full mr-2"><span class="font-bold">{{ itemNames[productIndex]['code'] }}</span> - {{ itemNames[productIndex]['name'] }} : </label>
+  <h1 class="mb-6">Moon Cake Distribution System</h1>
+
+  <!------------------------------->
+  <!--   Section Product Input   -->
+  <!------------------------------->
+  <div class="flex">
+    <div class="w-3">
+      <h4>Product Input：</h4>
+      <div class="flex m-1 w-full" v-for="productIndex of Object.keys(itemQuantities)">
+        <label class="w-8 mr-2"><span class="font-bold px-3">{{ itemNames[productIndex]['code'] }}</span> {{ itemNames[productIndex]['name'] }} : </label>
         <input v-model="itemQuantities[productIndex]" type="number" class="w-3rem"
                @focus="inputRef[productIndex].select();"
                ref="inputRef"
                @keyup.enter="initial"
               @blur="testBlur(productIndex)"/>
+        <div class="flex justify-content-end ml-4">
+          <input  readonly type="number" class="w-3rem bg-gray-100 " :value="itemDistributed[productIndex]"/>
+        </div>
       </div>
-      <div class="flex justify-content-end mt-4">
+      <Divider />
+      <div class="flex  m-1 font-bold">
+        <label class="w-8 mr-3 text-right"><span class="font-bold">Total: </span> </label>
+        <input readonly type="number" class="w-3rem" :value="totalToDistribute"/>
+        <div class="flex justify-content-end ml-4">
+          <input readonly type="number" class="w-3rem bg-gray-100 " :value="totalDistributed"/>
+        </div>
+      </div>
+      <div class="flex justify-content-center mt-4">
         <Button @click="initial">Distribute</Button>
       </div>
     </div>
+
+
     <Divider layout="vertical" />
-    <div class="ml-4">
+
+
+    <!------------------------------------>
+    <!--   Section Distributed Result   -->
+    <!------------------------------------>
+    <div class="ml-4 w-4">
       <h2>Distribute Result：</h2>
-      <div v-for="(box, index) in boxes" :key="index">
-        Box {{ index + 1 }} = {{ getBoxItemsNames(box).join(', ') }}
+      <div v-if="boxes.length > 0" class="text-2xl m-2" v-for="(box, index) in boxes" :key="index">
+        Box {{ index + 1 }} = <span class="font-bold">{{ getBoxItemsNames(box).join(', ') }} </span>
+      </div>
+      <div v-else>
+        <h3 class="text-2xl m-2 text-gray-500">-No Result-</h3>
+      </div>
+    </div>
+
+
+    <Divider layout="vertical" />
+
+    <!---------------------------------->
+    <!--   Section Distributed Item   -->
+    <!---------------------------------->
+    <div class="ml-4">
+      <h2>Item Distributed：</h2>
+      <div v-if="Object.keys(itemDistributed).length > 0" class="flex m-1" v-for="(count, index) in itemDistributed" :key="index">
+        <div class="w-3rem">{{ getItemNames(Number(index)) }}： </div>
+        <input readonly type="number" class="w-3rem" :value="count"/>
+      </div>
+      <div v-else>
+        <h3 class="text-2xl m-2 text-gray-500">-No Result-</h3>
+      </div>
+      <Divider />
+      <div class="flex m-1 font-bold">
+        <span class="pr-2">Total:</span>
+        <input readonly type="number" class="w-3rem" :value="totalDistributed"/>
       </div>
     </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
@@ -37,7 +85,7 @@ function testBlur(productIndex: number|string)
 }
 
 // 定義15種品項的名稱和數量
-const itemQuantities = reactive([13, 4, 4, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+const itemQuantities = reactive([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     inputRef = ref([]),
     itemNames = ref([
       // {code: 'A1', name: 'Taro Jingsa'},
@@ -71,11 +119,20 @@ const itemQuantities = reactive([13, 4, 4, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     // newFlavour = ref(['A2', 'A3', 'A4']),
     // pandanFlavour = ref(['E1', 'E2']),
     // mixedNutFlavour = ref(['C1', 'D1']),
+    totalToDistribute = computed(() => itemQuantities.reduce((itemQuantities, val) => itemQuantities + val)),
     boxCapacityOf4 = 4,
     boxCapacityOf2 = 2,
     itemsRemaining = ref<Array<any>>([]),
     boxes = ref<Array<any>>([]),
     totalBox = ref<number>(0),
+    itemDistributed = ref<Object>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+    totalDistributed = computed(() => {
+      let total = 0;
+      Object.values(itemDistributed.value).forEach((values) => {
+        total += values;
+      });
+      return total;
+    }),
     totalItems = computed(() => itemQuantities.reduce((sum, qty) => sum + qty, 0));
 
 onMounted(() => {
@@ -90,6 +147,7 @@ function getItemIdxByCode(code: string)
 function initial()
 {
   boxes.value = [];
+  itemDistributed.value = [];
   if (totalItems.value <= 2)
   {
     itemQuantities.forEach((element, index) => {
@@ -135,84 +193,70 @@ function initial()
     {
       boxes.value.push([]);
     }
-    console.log(totalBox.value, boxes.value, totalItems.value % boxCapacityOf4);
     itemsRemaining.value = [...itemQuantities];
-    // boxes.value = distribute2();
+    boxes.value = distribute();
   }
 }
-function distribute2()
-{
-  const retain = totalItems.value % boxCapacityOf4;
-  let boxArray: Array<any> = [], current = 0, itemToDistribute = [...itemQuantities];
-  do{
-    sequence.value.forEach((itemCode) => {
-      const itemIdx = getItemIdxByCode(itemCode);
-      do{
-        let currentIdx = current % totalBox.value;
-        if (boxArray[currentIdx] === undefined)
-          boxArray[currentIdx] = [];
-        if (boxArray[currentIdx].length >= boxCapacityOf4) {
-          current++;
-          continue;
-        }
-        if (boxArray[currentIdx].length < boxCapacityOf4) {
-          boxArray[currentIdx].push(itemIdx);
-          itemToDistribute[itemIdx]--;
-        }
-        current++;
-      }while(itemToDistribute[itemIdx] > 0)
-    });
-    for(let i = 0; i < boxes.value.length; i++)
-    {
-      if (boxArray[i] === undefined)
-        boxArray[i] = [];
-      for (let j = 0; j < itemToDistribute.length; j++) {
-        if (j === itemToDistribute.length - 1 && retain > 0 && boxArray[i].length >= retain) {
-          continue;
-        }
-        if (itemToDistribute[j] > 0 && boxArray[i].length < boxCapacityOf4) {
-          boxArray[i].push(j);
-          itemToDistribute[j]--;
-        }
-      }
-      if (boxArray[i].length >= boxCapacityOf4) {
-        boxArray[i].sort();
-      }
-    }
-
-
-
-    if (boxArray[current] === undefined)
-      boxArray[current] = [];
-    for (let j = 0; j < itemToDistribute.length; j++) {
-      if (itemToDistribute[j] > 0 && boxArray[current].length < boxCapacityOf4) {
-        boxArray[current].push(j);
-        itemToDistribute[j]--;
-      }
-    }
-    if (boxArray[current].length >= boxCapacityOf4) {
-      boxArray[current].sort();
-    }
-  }while(itemToDistribute.reduce((itemQuantities, val) => itemQuantities + val) > 0)
-
-  return boxArray;
-}
-
 function distribute()
 {
-  let boxArray: Array<any> = [], current = 0, itemToDistribute = [...itemQuantities];
+  const retain = totalItems.value % boxCapacityOf4;
+  let boxArray: Array<any> = [], current = 0, itemToDistribute = [...itemQuantities], sequenceFirst = false;
+
   do{
-    if (boxArray[current] === undefined)
-      boxArray[current] = [];
-    for (let j = 0; j < itemToDistribute.length; j++) {
-      if (itemToDistribute[j] > 0 && boxArray[current].length < boxCapacityOf4) {
-        boxArray[current].push(j);
-        itemToDistribute[j]--;
-      }
+    if (!sequenceFirst) {
+      sequenceFirst = true;
+      sequence.value.forEach((itemCode) => {
+        const itemIdx = getItemIdxByCode(itemCode);
+        console.log(itemCode, itemIdx, itemToDistribute[itemIdx], itemToDistribute.length);
+        do {
+          if(itemToDistribute[itemIdx] <= 0)
+            continue;
+          const currentBoxIdx = current % totalBox.value;
+          if (currentBoxIdx === boxArray.length - 1 && retain > 0 && boxArray[currentBoxIdx].length >= retain) {
+            console.log(boxArray[currentBoxIdx].length, boxArray[currentBoxIdx]);
+            current++;
+          } else {
+            if (boxArray[currentBoxIdx] === undefined)
+              boxArray[currentBoxIdx] = [];
+            if (boxArray[currentBoxIdx].length >= boxCapacityOf4) {
+              current++;
+            }else{
+              if (boxArray[currentBoxIdx].length < boxCapacityOf4 && itemToDistribute[itemIdx] > 0) {
+                boxArray[currentBoxIdx].push(itemIdx);
+                if (itemDistributed.value[itemIdx] === undefined)
+                  itemDistributed.value[itemIdx] = 0;
+                itemDistributed.value[itemIdx]++;
+                itemToDistribute[itemIdx]--;
+              }
+              current++;
+            }
+          }
+        } while (itemToDistribute[itemIdx] > 0)
+      });
     }
-    if (boxArray[current].length >= boxCapacityOf4) {
-      boxArray[current].sort();
-      current++;
+
+    for(let i = 0; i < boxes.value.length; i++)
+    {
+
+      if (i === boxArray.length - 1 && retain > 0 && boxArray[i].length >= retain)
+      {
+        break;
+      }else {
+        if (boxArray[i] === undefined)
+          boxArray[i] = [];
+        for (let j = 0; j < itemToDistribute.length; j++) {
+          if (itemDistributed.value[j] === undefined)
+            itemDistributed.value[j] = 0;
+          if (itemToDistribute[j] > 0 && boxArray[i].length < boxCapacityOf4) {
+            boxArray[i].push(j);
+            itemDistributed.value[j]++;
+            itemToDistribute[j]--;
+          }
+        }
+      }
+      // if (boxArray[i].length >= boxCapacityOf4) {
+      //   boxArray[i].sort();
+      // }
     }
   }while(itemToDistribute.reduce((itemQuantities, val) => itemQuantities + val) > 0)
 
@@ -221,6 +265,7 @@ function distribute()
 
 // 根據箱子中的商品索引，返回商品的名稱
 const getBoxItemsNames = (box: number[]) => box.map((itemIndex) => itemNames.value[itemIndex]['code']);
+const getItemNames = (itemIndex: number) => itemNames.value[itemIndex]['code'];
 
 </script>
 
